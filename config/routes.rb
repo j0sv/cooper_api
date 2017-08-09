78e802f7776d801ea87eb73c86ea8306afb359ca
j0sv <svensson.jonas@gmail.com>
2017-08-09 17:00:17 +0200

    Modify PerformanceData model and test

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ mount_devise_token_auth_for 'User', at: 'auth'
     namespace :v0 do
       resources :ping, only: [:index], constraints: { format: 'json' }
     end
-    namespace :v1 do
+    namespace :v1, defaults: { format: :json } do
       mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
+      resources :performance_data, only: [:create]
     end
   end
 end
--- spec/models/performance_data_spec.rb
+++ spec/models/performance_data_spec.rb
@@ -10,3 +10,20 @@ RSpec.describe PerformanceData, type: :model do
     it { is_expected.to belong_to :user }
   end
 end
+
+RSpec.describe Api::V1::PerformanceDataController, type: :request do
+  let(:user) { FactoryGirl.create(:user) }
+  let(:credentials) { user.create_new_auth_token }
+  let(:headers) { { HTTP_ACCEPT: 'application/json' }.merge!(credentials) }
+
+  describe 'POST /api/v1/performance_data' do
+    it 'creates a data entry' do
+      post '/api/v1/performance_data', params: {
+          performance_data: { data: { message: 'Average' } }
+      }, headers: headers
+
+      entry = PerformanceData.last
+      expect(entry.data).to eq 'message' => 'Average'
+    end
+  end
+end
